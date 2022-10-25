6cbf590505dca150f907c41796eb57ff86e9527e
Mnotho Gumede <mgumede@jhb.dvt.co.za>
2022-10-25 13:41:22 +0200

    tests

---
+++ test/setup.ts
@@ -0,0 +1,20 @@
+import 'react-native-gesture-handler/jestSetup';
+import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock';
+
+jest.mock('react-native-reanimated', () => {
+  const Reanimated = require('react-native-reanimated/mock');
+  Reanimated.default.call = () => {};
+  return Reanimated;
+});
+
+jest.mock('redux-persist', () => {
+  const real = jest.requireActual('redux-persist');
+  return {
+    ...real,
+    persistReducer: jest
+      .fn()
+      .mockImplementation((config, reducers) => reducers),
+  };
+});
+
+jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
